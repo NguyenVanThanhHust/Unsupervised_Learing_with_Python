308e9c3b85b3ae341006faabe2f7451fcedd1d3c
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2022-05-08 11:54:23 +0700

    Chapter 4

--- Chapter_4_Anomaly_Detection/Chapter_4_Anomaly_Detection.py
+++ Chapter_4_Anomaly_Detection/Chapter_4_Anomaly_Detection.py
@@ -3,6 +3,9 @@ from os.path import join, isdir, isfile
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
 from sklearn import preprocessing as pp
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import precision_recall_curve, average_precision_score
@@ -11,34 +14,28 @@ from sklearn.metrics import roc_curve, auc, roc_auc_score
 DATA_PATH = "../../creditcard.csv"
 data = pd.read_csv(DATA_PATH)
 
-print(data.head())
-
-print(data.describe())
-
 dataX = data.copy().drop(["Class"], axis=1)
 dataY = data["Class"].copy()
 
-X_train, y_train, X_test, y_test = train_test_split.(dataX, dataY, test_size=0.33, \
+X_train, X_test, y_train, y_test = train_test_split(dataX, dataY, test_size=0.33, \
                                         random_state=2018, stratify=dataY)
 
+# Calculate reconstruction error
 def anomalyScores(originalDF, reducedDF):
-    """
-    calcualte anomaly scores, that is diff
-    """
-    loss = np.sum(np.array(originalDF) - np.array(reducedDF)**2, axis=1)
-    loss = pd.Series(data=loss, index=originalDF.index)
-    loss = (loss - np.min(loss))/(np.max(loss) - np.min(loss))
+    loss = np.sum((np.array(originalDF)-np.array(reducedDF))**2, axis=1)
+    loss = pd.Series(data=loss,index=originalDF.index)
+    loss = (loss-np.min(loss))/(np.max(loss)-np.min(loss))
     return loss
 
 # Plot results
-def plotResults(trueLabels, anomalyScores, returnPreds = False):
+def plotResults(trueLabels, anomalyScores, algo_name, returnPreds = False):
     preds = pd.concat([trueLabels, anomalyScores], axis=1)
     preds.columns = ['trueLabel', 'anomalyScore']
     precision, recall, thresholds = \
         precision_recall_curve(preds['trueLabel'],preds['anomalyScore'])
     average_precision = \
         average_precision_score(preds['trueLabel'],preds['anomalyScore'])
-
+    
     plt.step(recall, precision, color='k', alpha=0.7, where='post')
     plt.fill_between(recall, precision, step='post', alpha=0.3, color='k')
 
@@ -46,13 +43,15 @@ def plotResults(trueLabels, anomalyScores, returnPreds = False):
     plt.ylabel('Precision')
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.0])
-
+    
     plt.title('Precision-Recall curve: Average Precision = \
     {0:0.2f}'.format(average_precision))
 
     fpr, tpr, thresholds = roc_curve(preds['trueLabel'], \
                                      preds['anomalyScore'])
     areaUnderROC = auc(fpr, tpr)
+    plt.savefig(algo_name+"_prec_reca.jpg")
+    plt.clf()
 
     plt.figure()
     plt.plot(fpr, tpr, color='r', lw=2, label='ROC curve')
@@ -64,9 +63,51 @@ def plotResults(trueLabels, anomalyScores, returnPreds = False):
     plt.title('Receiver operating characteristic: \
     Area under the curve = {0:0.2f}'.format(areaUnderROC))
     plt.legend(loc="lower right")
-    plt.savefig("result.jpg")
-
+    plt.savefig(algo_name+"_results.jpg")
+    
     if returnPreds==True:
         return preds
 
-print(isfile(DATA_PATH))
+
+# View scatterplot
+def scatterPlot(xDF, yDF, algoName):
+    tempDF = pd.DataFrame(data=xDF.loc[:,0:1], index=xDF.index)
+    tempDF = pd.concat((tempDF,yDF), axis=1, join="inner")
+    tempDF.columns = ["First Vector", "Second Vector", "Label"]
+    sns.lmplot(x="First Vector", y="Second Vector", hue="Label", \
+               data=tempDF, fit_reg=False)
+    ax = plt.gca()
+    ax.set_title("Separation of Observations using "+algoName)
+
+    plt.savefig(algoName+".png")
+
+
+## PCA 
+from sklearn.decomposition import PCA 
+
+# define some hyperparameters
+n_components = 30 # same as in credit card data
+whiten = False
+random_state = 2018
+
+pca = PCA(n_components=n_components, whiten=whiten, random_state=random_state)
+
+x_train_PCA = pca.fit_transform(X_train)
+x_train_PCA = pd.DataFrame(data=x_train_PCA, index=X_train.index)
+
+x_train_PCA_inverse = pca.inverse_transform(x_train_PCA)
+x_train_PCA_inverse = pd.DataFrame(data=x_train_PCA_inverse, index=x_train_PCA.index)
+
+scatterPlot(x_train_PCA, y_train, "PCA")
+anomalyScoresPCA = anomalyScores(x_train_PCA, x_train_PCA_inverse)
+preds = plotResults(y_train, anomalyScoresPCA, "PCA", True)
+
+# Analyze results
+preds.sort_values(by="anomalyScore",ascending=False,inplace=True)
+cutoff = 350
+predsTop = preds[:cutoff]
+print("Precision: ",np.round(predsTop. \
+            anomalyScore[predsTop.trueLabel==1].count()/cutoff,2))
+print("Recall: ",np.round(predsTop. \
+            anomalyScore[predsTop.trueLabel==1].count()/y_train.sum(),2))
+print("Fraud Caught out of 330 Cases:", predsTop.trueLabel.sum())
